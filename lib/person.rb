badde803949c5aa305f6fb2dacfad69e8be62653
github username <github email address>
2019-10-29 16:24:19 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,24 @@
+class Person
+
+def initlialize()
+    @name = nil
+end 
+
+def name
+    @name
+end 
+
+def name=(human_name)
+    @name=human_name
+end 
+
+def job
+    @job
+end 
+
+def job=(human_job)
+    @job = human_job
+end 
+
+
+end
